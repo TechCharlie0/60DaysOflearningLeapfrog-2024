0a37c63d10683e2b3c24068896336cd3d6c7fa82
isha kandel <ishachttri@gmail.com>
2024-06-13 18:52:23 +0545

    unary

---
+++ Day 13/AssigmentOperaot.js
@@ -0,0 +1,10 @@
+//ASSIGN THE VALUE IN SHORT FORM: ASSIGMENT OPRATOR
+let age = 32;
+console.log((age += 10));
+console.log((age -= 10));
+console.log((age *= 10));
+
+//UNARY OPERATOR
+let age1 = 24;
+console.log(age1++); //Postincrement operator
+console.log(++age1); //Preincrement operator
