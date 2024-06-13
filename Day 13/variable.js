27262619b6d7ce640233762a84dfc48a4e4207f0
isha kandel <ishachttri@gmail.com>
2024-06-13 18:10:48 +0545

    variable

--- Day 13/variable.js
+++ Day 13/variable.js
@@ -1,1 +1,13 @@
-!
+//using console log
+console.log("This my first JavaScript class");
+
+//using variable in JS
+var a = 5; //int
+var b = 6;
+console.log(a + b);
+
+//To check the typeof Variable
+var m = 7;
+var n = 6;
+var o = "Isha";
+console.log(typeof m, typeof n, typeof o);
