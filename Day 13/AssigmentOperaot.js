4e441e776cdeac35d0c552f0c49759f144277262
isha kandel <ishachttri@gmail.com>
2024-06-13 18:54:24 +0545

    ok

--- Day 13/AssigmentOperaot.js
+++ Day 13/AssigmentOperaot.js
@@ -6,5 +6,5 @@ console.log((age *= 10));
 
 //UNARY OPERATOR
 let age1 = 24;
-console.log(age1++); //Postincrement operator
 console.log(++age1); //Preincrement operator
+console.log(age1++); //Postincrement operator
