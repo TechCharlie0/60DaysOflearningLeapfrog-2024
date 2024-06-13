61ed9fe1208ba3ca76a3ae3bb6a46c1ca1aec716
isha kandel <ishachttri@gmail.com>
2024-06-13 18:19:55 +0545

    var and let

--- Day 13/variable.js
+++ Day 13/variable.js
@@ -11,3 +11,17 @@ var m = 7;
 var n = 6;
 var o = "Isha";
 console.log(typeof m, typeof n, typeof o);
+
+//using constant in JS
+const a1 = 8;
+a2 = a1 + 1;
+console.log(a2);
+
+//using let in JS insted of variable
+let i = 3;
+let j = 6;
+{
+  let i = 33;
+  console.log(i); //let is block scope
+}
+console.log(i);
