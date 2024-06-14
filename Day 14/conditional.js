67325cfcba1e30c280d82ec03d48768bba3207ae
isha kandel <ishachttri@gmail.com>
2024-06-14 16:58:17 +0545

    else

--- Day 14/conditional.js
+++ Day 14/conditional.js
@@ -10,3 +10,13 @@ if (color === "yellow") {
 if (color === "green") {
   console.log("go");
 }
+
+//else-if statement
+let age = 14;
+if (age >= 18) {
+  console.log("you can vote");
+} else if (age <= 18) {
+  console.log("you cannot vote");
+}
+
+//else statement
