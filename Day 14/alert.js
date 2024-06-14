d52afc54d8481ab4774dd44ee6de9933bae39dd0
isha kandel <ishachttri@gmail.com>
2024-06-14 17:19:10 +0545

    prompt alert

---
+++ Day 14/alert.js
@@ -0,0 +1,6 @@
+//alert
+alert("this is simple allert to refill your detail");
+//promot
+let firstName = prompt("enter your first name");
+let lastName = prompt("enter your last name");
+console.log(`My name is ${firstName + lastName}`);
--- Day 14/script.js
+++ Day 14/script.js
@@ -9,3 +9,25 @@ console.log(`The total price is : ${pencilPrice + eraserPrice}rupees `); //after
 let firstName = "isha";
 let lastName = "kandel";
 console.log(`My name is ${firstName + lastName}`); //after using the template literal in js
+
+//logical operator
+let marks = 75;
+if ((marks > 33 && marks <= 80) || !false) {
+  console.log("pass");
+}
+
+//switch statement
+let color = "red";
+switch (color) {
+  case "red":
+    console.log("stop");
+    break;
+  case "yellow":
+    console.log("slow down");
+    break;
+  case "green":
+    console.log("go");
+    break;
+  default:
+    console.log("broken light");
+}
