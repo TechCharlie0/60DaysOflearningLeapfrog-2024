4c1cb36b0a267227e836645465427708eda5daf7
isha kandel <ishachttri@gmail.com>
2024-06-14 16:36:11 +0545

    template literal

---
+++ Day 14/script.js
@@ -0,0 +1,9 @@
+//Tempelate leteral in JS
+
+let pencilPrice = 10;
+let eraserPrice = 30;
+console.log("The total price is:", pencilPrice + eraserPrice, "Rupees");
+
+//after using the template literal in js
+
+console.log(`The total price is : ${pencilPrice + eraserPrice}rupees `);
