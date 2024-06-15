e1dd99c379b73529902dc8c26653ee8b6e620b89
isha kandel <ishachttri@gmail.com>
2024-06-15 20:17:27 +0545

    replace

--- Day 15/string.js
+++ Day 15/string.js
@@ -15,3 +15,17 @@ console.log(str.toLowerCase());
 //string methods with argument
 console.log(str.indexOf("kandel")); //1(found)
 console.log(str.indexOf("j")); //-1(not found)
+
+//method chaining
+let string = "hello";
+console.log(string.trim().toUpperCase());
+
+//slice method
+let slic = "ilovecoding";
+console.log(slic.slice(5)); //coding
+console.log(slic.slice(1, 4)); //love
+console.log(slic.slice(-1)); //g
+
+//replace
+console.log(slic.replace("love", "like")); //ilikecoding
+console.log(slic.replace("l", "m")); //impvecoding
