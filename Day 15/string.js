403e6d7e34715d263aeb0e0addb499567744d44e
isha kandel <ishachttri@gmail.com>
2024-06-15 20:06:58 +0545

    indexof

--- Day 15/string.js
+++ Day 15/string.js
@@ -1,3 +1,17 @@
 //trim method
 let msg = "  hello   ";
 console.log(msg.trim());
+
+let password = prompt("enter you new password");
+console.log(password.trim());
+
+//in js string are immutable that means a new string is created and old one remains same
+
+//to string into toUppercase and toLowercase
+let str = "ishakandel";
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+//string methods with argument
+console.log(str.indexOf("kandel")); //1(found)
+console.log(str.indexOf("j")); //-1(not found)
