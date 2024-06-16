0fc3d081268f9b9d025c3cd77e698ec626646572
isha kandel <ishachttri@gmail.com>
2024-06-16 16:26:22 +0545

    array

---
+++ Day 16/script.js
@@ -0,0 +1,14 @@
+//creating array
+let strs = ["isha", "kandel", 12];
+console.log(strs[0].length);
+
+//array are mutable
+
+let fruit = ["mango", "apple", "litchi"];
+fruit[0] = "banana";
+
+//array method
+
+//push
+let str = ["isha", "kandel", "robert", "pattinson"];
+console.log(str.push("lover"));
