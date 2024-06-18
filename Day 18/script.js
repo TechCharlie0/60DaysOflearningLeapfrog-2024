60ff4827ea76f6168ace468f9b7e7e7e96289d7e
isha kandel <ishachttri@gmail.com>
2024-06-18 15:39:32 +0545

    loop with array

--- Day 18/script.js
+++ Day 18/script.js
@@ -9,8 +9,21 @@ for (let i = 0; i <= 10; i = i + 2) {
 }
 
 //nested for loop
-for (let i = 0; i < 3; i++) {
+for (let i = 1; i < 3; i++) {
   for (let j = 1; j <= 3; j++) {
     console.log(j);
   }
+}
+
+//while loop
+let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+//loops with array
+let fruits = ["mango", "apple", "banana", "litchi", "orange"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(i, fruits[i]);
 }
