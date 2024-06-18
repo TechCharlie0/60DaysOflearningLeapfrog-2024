4d45e7eea658ea8712576743fec86f597ba20443
isha kandel <ishachttri@gmail.com>
2024-06-18 15:35:43 +0545

    nested loop

--- Day 18/script.js
+++ Day 18/script.js
@@ -1,3 +1,16 @@
+//for loop
 for (let i = 1; i <= 15; i++) {
   console.log(i);
+}
+
+//print all even number
+for (let i = 0; i <= 10; i = i + 2) {
+  console.log(i);
+}
+
+//nested for loop
+for (let i = 0; i < 3; i++) {
+  for (let j = 1; j <= 3; j++) {
+    console.log(j);
+  }
 }
