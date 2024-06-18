52d9c9e4f87919685e54cbbea4a774e30c5afa87
isha kandel <ishachttri@gmail.com>
2024-06-18 15:48:32 +0545

    nested for of

--- Day 18/nested.js
+++ Day 18/nested.js
@@ -16,3 +16,18 @@ let fruits = ["mango", "apple", "banana", "litchi", "orange"];
 for (fruit of fruits) {
   console.log(fruit);
 }
+//string
+for (char of "keapfrog") {
+  console.log(char);
+}
+
+//nested for of loop
+let hero = [
+  ["ironman", "spiderman", "thor"],
+  ["superman", "wonderwoman", "flash"],
+];
+for (list of hero) {
+  for (hero of list) {
+    console.log(hero);
+  }
+}
