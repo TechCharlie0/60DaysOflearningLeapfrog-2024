a8ba207accca5c71133fbba9ee04483456c747dc
isha kandel <ishachttri@gmail.com>
2024-06-20 18:48:14 +0545

    method function

--- Day 20/function.js
+++ Day 20/function.js
@@ -3,3 +3,30 @@ const sum = function (a, b) {
   return a + b;
 };
 sum(2, 3);
+
+//Highrt Order Function: A function that does one or both
+//takes one or multiple functions as arguments
+function multipleGreet(func, n) {
+  for (let i = 1; i <= n; i++) {
+    func();
+  }
+}
+let greet = function () {
+  console.log("hello");
+};
+multipleGreet(greet, 2); //result:hello hello
+
+//return a function
+function oddEvenTest(request) {
+  if (request == "odd") {
+    return function (n) {
+      console.log(!(n % 2 == 0));
+    };
+  } else if (request == "even") {
+    return function (n) {
+      console.log(n % 2 == 0);
+    };
+  } else {
+    console.log("wrong request");
+  }
+}
--- Day 20/scriot.js
+++ Day 20/scriot.js
@@ -40,3 +40,16 @@ function outerFunc() {
   }
   innerFunc();
 }
+
+//Methods:Actios that can be performed on an object
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  },
+  mul: function (a, b) {
+    return a * b;
+  },
+};
