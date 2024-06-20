f28aab18325620801094f32775317fb5c1c5d338
isha kandel <ishachttri@gmail.com>
2024-06-20 18:02:28 +0545

    function

---
+++ Day 20/index.js
@@ -0,0 +1,15 @@
+//function
+//function defination(telling the js)
+function isha() {
+  console.log("function calling"); //do something
+}
+//function calling(using the function)
+isha();
+
+//Question: create a function that prints a poem
+function printPoem() {
+  console.log("Twilking Twinkiling litter star");
+}
+printPoem();
+
+//Question:Create a function to roll a dice and always display the value of the dice
