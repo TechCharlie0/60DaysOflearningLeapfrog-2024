07a1b7372e9249f6653926667d1a6e7dd33abcae
isha kandel <ishachttri@gmail.com>
2024-06-23 18:54:41 +0545

    map

--- Day 23/foeach.js
+++ Day 23/foeach.js
@@ -1,6 +1,24 @@
 //forEach:arr.forEach(some function definition or nam);
-let arr = [1, 2, 3, 4, 5];
-function print(el) {
+let array = [1, 2, 3, 4, 5];
+let print = function (el) {
   console.log(el);
-}
+};
 arr.forEach(print);
+
+let arr = [
+  {
+    name: "isha",
+    marks: 97,
+  },
+  {
+    name: "robert",
+    marks: 95,
+  },
+  {
+    name: "Starc",
+    marks: 93,
+  },
+];
+arr.forEach((student) => {
+  console.log(student.marks);
+});
