3d39a2588d4435cb550818102a947a107164b543
isha kandel <ishachttri@gmail.com>
2024-06-23 19:30:33 +0545

    filter

--- Day 23/foeach.js
+++ Day 23/foeach.js
@@ -22,3 +22,13 @@ let arr = [
 arr.forEach((student) => {
   console.log(student.marks);
 });
+
+//map: let newArr = arr.map(some function definition or name)
+let num = [1, 2, 3, 4];
+let double = num.map(function (el) {
+  return el * 2;
+});
+
+//finter: let newArr = arr.filter(some function definition or name);
+let nums = [2, 4, 1, 5, 6, 2, 7, 8, 9];
+let even = nums.filter((num) => num % 2 == 0);
