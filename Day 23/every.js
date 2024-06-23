423e607a41bf6d220284a492604dd59cdf6752c9
isha kandel <ishachttri@gmail.com>
2024-06-23 19:56:28 +0545

    reduce

--- Day 23/every.js
+++ Day 23/every.js
@@ -4,3 +4,9 @@ let arr = [1, 2, 3, 4];
 arr.every((el) => el % 2 == 0); //false
 let arr1 = [2, 4];
 arr1.every((el) => el % 2 == 0); //true
+
+//reduce:reduce the array to a single value
+//arr.reduce(reducer function with 2 variaboe fo(accumulator,element);
+
+let odd = [1, 2, 3, 4];
+odd.reduce((res, el) => res + el);
