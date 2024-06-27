73b5f91c3b14b91ee5f3ae43f207dffaf396bfe4
isha kandel <ishachttri@gmail.com>
2024-06-27 22:16:22 +0545

    this

--- Day 27/event.js
+++ Day 27/event.js
@@ -14,3 +14,12 @@ let btn = document.querySelector("button");
 btn.addEventListener("click", function () {
   console.log("button was clicled");
 });
+
+//this is eventlisteners
+//when 'this' is used in ca callback of event handler of domething it refers to that something
+
+let bot = document.querySelector("button");
+btn.addEventListener("click", function () {
+  console.log(this.innerText);
+  this.style.backgroundColor = "pink";
+});
