01d0ee51c29fc3d01f9b2db11f1050e45f283eef
isha kandel <ishachttri@gmail.com>
2024-06-27 22:28:00 +0545

    extracting from data

--- Day 27/form.js
+++ Day 27/form.js
@@ -3,5 +3,12 @@
 let form = document.querySelector("form");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  alert("form submitted");
+  aconsole.log("form submitted");
 });
+
+//extracting form data
+let user = this.Element[0];
+let pass = this.Element[1];
+console.log(user.value);
+console.log(pass.value);
+alert(`hi ${user.value}, your password is set to ${pass.value}`);
