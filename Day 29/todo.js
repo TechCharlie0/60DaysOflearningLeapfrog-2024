39fb06f33f79557d384d7c55a526cfd02eaf9e13
isha kandel <ishachttri@gmail.com>
2024-06-29 19:59:35 +0545

    todo complete

--- Day 29/todo.js
+++ Day 29/todo.js
@@ -4,7 +4,20 @@ let inp = document.querySelector("input");
 
 btn.addEventListener("click", function () {
   let item = document.createElement("li");
+
   item.innerText = inp.value;
+  let delBtn = document.createElement("button");
+  delBtn.innerText = "delete";
+  delBtn.classList.add("delete");
+  item.appendChild(delBtn);
   ul.appendChild(item);
   inp.value = "";
 });
+
+let delBtns = document.querySelectorAll(".delete");
+for (delBtn of delBtns) {
+  delBtn.addEventListener("click", function () {
+    let par = this.parentElement;
+    par.remove();
+  });
+}
