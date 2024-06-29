cac41265e1fb19fa7a06550904387e8533e2ccd3
isha kandel <ishachttri@gmail.com>
2024-06-29 19:19:52 +0545

    triggered

---
+++ Day 29/script.js
@@ -0,0 +1,12 @@
+let div = document.querySelector("div");
+let ul = document.querySelector("ul");
+let lis = document.querySelector("li");
+div.addEventListener("click", function () {
+  console.log("we clicked it");
+});
+ul.addEventListener("click", function () {
+  console.log("we clicked it");
+});
+lis.addEventListener("click", function () {
+  console.log("we clicked it");
+});
