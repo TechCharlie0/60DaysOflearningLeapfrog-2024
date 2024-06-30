e3870447e8b173ae33205623e1aecb360821f968
isha kandel <ishachttri@gmail.com>
2024-06-30 18:58:00 +0545

    promise

--- Day 30/promise.js
+++ Day 30/promise.js
@@ -1,2 +1,14 @@
-//promises
-//the promise object reperesent the eventual completion of an asynchronous operation and its resulting value
+//promises: the promise object reperesent the eventual completion of an asynchronous operation and its resulting value
+//resolve and reject
+//asynchronous data
+
+function saveToDatabase(data) {
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+      resolve("sucess");
+    } else {
+      reject("failure");
+    }
+  });
+}
