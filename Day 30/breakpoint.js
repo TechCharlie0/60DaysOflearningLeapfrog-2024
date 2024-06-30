faf9de02d3d1158bd5451f4da4f85abb1ada68f9
isha kandel <ishachttri@gmail.com>
2024-06-30 18:55:35 +0545

    sucess

--- Day 30/breakpoint.js
+++ Day 30/breakpoint.js
@@ -12,3 +12,61 @@ setTimeout(() => {
   console.log("Robert pattinson");
 }, 2000);
 console.log("hello..");
+
+//callback hell
+h1 = document.querySelector("h1");
+function changecolor(color, delay, nextcolorchange) {
+  setTimeout(() => {
+    h1.style.color = color;
+    if (nextcolorchange) nextcolorchange();
+  }, delay);
+}
+
+changecolor("red", 1000, () => {
+  changecolor("orange", 1000, () => {
+    changecolor("blue", 1000, () => {
+      changecolor("yellow", 1000, () => {
+        changecolor("purple", 1000);
+      });
+    });
+  });
+});
+
+//or
+
+function saveToDatabase(data, sucess, failure) {
+  let internetSpeed = Math.floor(Math.random() * 10) + 1;
+  if (internetSpeed > 4) {
+    sucess();
+  } else {
+    failure();
+  }
+}
+
+saveToDatabase(
+  "apna college",
+  () => {
+    console.log("sucess");
+    saveToDatabase(
+      "hello world",
+      () => {
+        console.log("sucess2");
+        saveToDatabase(
+          "isha",
+          () => {
+            console.log("sucess3");
+          },
+          () => {
+            console.log("failure3");
+          }
+        );
+      },
+      () => {
+        console.log("fail2 ");
+      }
+    );
+  },
+  () => {
+    console.log("failure");
+  }
+);
---
+++ Day 30/promise.js
@@ -0,0 +1,2 @@
+//promises
+//the promise object reperesent the eventual completion of an asynchronous operation and its resulting value
