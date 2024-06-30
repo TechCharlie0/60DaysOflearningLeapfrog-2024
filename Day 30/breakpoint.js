ec1e39c27e602536884180ba6f143602f3361369
isha kandel <ishachttri@gmail.com>
2024-06-30 18:49:29 +0545

    settimeout

--- Day 30/breakpoint.js
+++ Day 30/breakpoint.js
@@ -1,13 +1,14 @@
 //js is single threaded
-
 let a = 25;
 console.log(a);
 let b = 10;
 console.log(b);
 console.log(a + b);
-
 //or
-setTimeout(function () {
+setTimeout(() => {
   console.log("isha kandel");
+}, 2000);
+setTimeout(() => {
+  console.log("Robert pattinson");
 }, 2000);
 console.log("hello..");
