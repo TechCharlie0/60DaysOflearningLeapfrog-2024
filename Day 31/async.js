14fe4ed9c01635364bc3f09c5530f759b931adf7
isha kandel <ishachttri@gmail.com>
2024-07-01 17:44:51 +0545

    async

--- Day 31/async.js
+++ Day 31/async.js
@@ -5,3 +5,23 @@ async function greet(){
     return "hello world"; //return a promise
 }
 let hello = async()={}; //return a promise
+
+//or
+async function greet() {
+      throw"some randowm error";
+       return "hello"; // return a promise
+     }
+
+     greet()
+     .then((result)=>{
+        console.log("promise was resolved");
+        console.log("result was:", result);
+     })
+     .catch(err)=>{
+        console.log("promise was rejected with err:",err);
+     }
+
+//await keyword:pauses the execution of its surrounding async function until the promise is settles (resolved or rejected)
+
+
+    
