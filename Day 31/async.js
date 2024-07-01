54eaa3afbbfa981ab37afe832f492ad9405c03c1
isha kandel <ishachttri@gmail.com>
2024-07-01 17:46:04 +0545

    async with promises

--- Day 31/async.js
+++ Day 31/async.js
@@ -1,27 +1,33 @@
 //async keyword
 //create an async function
 
-async function greet(){
+async function greet() {
     return "hello world"; //return a promise
 }
-let hello = async()={}; //return a promise
+let hello = async() = {}; //return a promise
 
 //or
 async function greet() {
-      throw"some randowm error";
-       return "hello"; // return a promise
-     }
+    throw "some randowm error";
+    return "hello"; // return a promise
+}
 
-     greet()
-     .then((result)=>{
+greet()
+    .then((result) => {
         console.log("promise was resolved");
         console.log("result was:", result);
-     })
-     .catch(err)=>{
-        console.log("promise was rejected with err:",err);
-     }
+    })
+    .catch(err)=> {
+    console.log("promise was rejected with err:", err);
+}
 
 //await keyword:pauses the execution of its surrounding async function until the promise is settles (resolved or rejected)
+function getNum() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let num = Math.floor(Math.random() * 10) + 1;
+            resolve(num);
+        }, 1000);
+    });
+}
 
-
-    
