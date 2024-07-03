9a2c154f78f6c758a02ee92b9d320d1cf7119d76
isha kandel <ishachttri@gmail.com>
2024-07-03 19:33:27 +0545

    js feature

--- Day 33/script.js
+++ Day 33/script.js
@@ -1,11 +1,21 @@
-//axios:library to make http request
+let btn = document.querySelector("#factButton");
+btn.addEventListener("click", async () => {
+  btn.innerText = "Fetching...";
+  let fact = await getFacts();
+  console.log(fact);
+  let p = document.querySelector("#result");
+  p.innerText = fact;
+  btn.innerText = "Show new fact";
+});
+
 let url = "https://catfact.ninja/fact";
 
 async function getFacts() {
   try {
     let res = await axios.get(url);
-    console.log(res);
+    return res.data.fact;
   } catch (e) {
     console.log("error-", e);
+    return "No fact found";
   }
 }
