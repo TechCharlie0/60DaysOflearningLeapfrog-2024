ea3044ee2da72c529f933cc8b0436cd2005a7ce5
isha kandel <ishachttri@gmail.com>
2024-07-04 18:36:16 +0545

    js added done

--- Day 34/script.js
+++ Day 34/script.js
@@ -1,17 +1,26 @@
 let btn = document.querySelector("#factButton");
 let url2 = "https://dog.ceo/api/breeds/image/random";
 btn.addEventListener("click", async () => {
-  let link = await getImages();
-  let img = document.querySelector("#result");
+  let link = await getImage();
+  let resultContainer = document.querySelector("#result");
+
+  // Clear previous content
+  resultContainer.innerHTML = "";
+
+  // Create new img element
+  let img = document.createElement("img");
   img.setAttribute("src", link);
+  resultContainer.appendChild(img);
+
   console.log(link);
 });
-async function getImages() {
+
+async function getImage() {
   try {
     let res = await axios.get(url2);
     return res.data.message;
   } catch (e) {
     console.log("error-", e);
-    return "No fact found";
+    return "No image found";
   }
 }
