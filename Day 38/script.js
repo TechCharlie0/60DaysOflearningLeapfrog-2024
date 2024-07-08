4b7ab640d11780489ca4648319d7f829846b663a
isha kandel <ishachttri@gmail.com>
2024-07-08 18:34:28 +0545

    autoplay ready

--- Day 38/script.js
+++ Day 38/script.js
@@ -1,6 +1,9 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
-  spaceBetween: 30,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 1500,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
