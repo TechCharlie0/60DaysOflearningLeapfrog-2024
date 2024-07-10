37d577f127d735e63b1f15679e97c042c1507218
isha kandel <ishachttri@gmail.com>
2024-07-10 12:33:10 +0545

    off set height

--- Day 40/script.js
+++ Day 40/script.js
@@ -2,7 +2,7 @@ const nav = document.querySelector(".nav");
 window.addEventListener("scroll", fixNav);
 
 function fixNav() {
-  if (window.scrollY > nav.offsetHeight + 150) {
+  if (window.scrollY > nav.offsetHeight + 200) {
     nav.classList.add("active");
   } else {
     nav.classList.remove("active");
