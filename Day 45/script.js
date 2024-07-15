3d716b6cec15aedc82e1793adb437686dcee9d29
isha kandel <ishachttri@gmail.com>
2024-07-15 16:14:41 +0545

    making ready

--- Day 45/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById("menu");
-const navigation = document.getElementById("navigation");
-hamburger.addEventListener("click", function () {
-  navigation.classList.toggle("active");
-  console.log("clicked");
-});
