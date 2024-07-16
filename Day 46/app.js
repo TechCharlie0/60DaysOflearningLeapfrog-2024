866622ea859c600df2e5f44e3807e1f2f2595f0a
isha kandel <ishachttri@gmail.com>
2024-07-16 11:06:09 +0545

    js adding and transition

--- Day 46/app.js
+++ Day 46/app.js
@@ -1,13 +1,13 @@
-const loginForm = document.querySelector(".login-form");
-const signupForm = document.querySelector(".signup-form");
-const loginBtn = document.querySelector(".login button");
-const signupBtn = document.querySelector(".signup button");
-const backLayer = document.querySelector(".back-layer");
+let loginForm = document.querySelector(".login-form");
+let signupForm = document.querySelector(".signup-form");
+let loginBtn = document.querySelector(".login button");
+let signupBtn = document.querySelector(".signup button");
+let backLayer = document.querySelector(".back-layer");
 
 signupBtn.addEventListener("click", () => {
   loginForm.classList.remove("active");
   signupForm.classList.add("active");
-  backLayer.style.clipPath = "inset(0 0 0 50%)";
+  backLayer.style.clipPath = "";
 });
 
 loginBtn.addEventListener("click", () => {
