ff3dd7726c645153b7b7d492a4c3c60e26350a84
isha kandel <ishachttri@gmail.com>
2024-07-19 11:58:46 +0545

    js is ready

--- Day 49/app.js
+++ Day 49/app.js
@@ -7,11 +7,13 @@ function validate() {
   let pattern = /^[^]+@[^]+\.[a-z]{2,3}$/;
 
   if (input.value.match(pattern)) {
-    form.classList.add(console.log(your password is set:"valid"));
+    form.classList.add("valid");
     form.classList.remove("invalid");
+    console.log("Email is set correctly.");
+    alert("Email is set correctly.");
   } else {
     form.classList.add("invalid");
     form.classList.remove("valid");
-
+    alert("Please fill in the correct form.");
   }
 }
