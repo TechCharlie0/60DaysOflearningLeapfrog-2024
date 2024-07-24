30007106489ab96c6c338ea29787d3e3de7a5dc5
isha kandel <ishachttri@gmail.com>
2024-07-24 06:43:36 +0545

    done

--- Day 54/script.js
+++ Day 54/script.js
@@ -4,6 +4,7 @@ MenuItems.style.maxHeight = "0px";
 function menutoggle() {
   if (MenuItems.style.maxHeight == "0px") {
     MenuItems.style.maxHeight = "200px";
+  } else {
     MenuItems.style.maxHeight = "0px";
   }
 }
