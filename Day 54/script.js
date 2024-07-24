775d2ea011f3eb45f078a7facfd828a508a6550b
isha kandel <ishachttri@gmail.com>
2024-07-24 06:43:18 +0545

    if else

--- Day 54/script.js
+++ Day 54/script.js
@@ -4,7 +4,6 @@ MenuItems.style.maxHeight = "0px";
 function menutoggle() {
   if (MenuItems.style.maxHeight == "0px") {
     MenuItems.style.maxHeight = "200px";
-  } else {
     MenuItems.style.maxHeight = "0px";
   }
 }
